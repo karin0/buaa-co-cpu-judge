70bc6c49bc5f15e0f8d9830209df5a231c3ba7e0
karin0 <karin0@gmx.com>
2020-11-08 17:36:33 +0800

    feat: add timeout for subprocess

--- README.md
+++ README.md
@@ -1,32 +1,36 @@
 This is inspired by [
 BUAA-CO-Logisim-Judger](https://github.com/biopuppet/BUAA-CO-Logisim-Judger).
 
-This helps to verify MIPS CPU circuits in Logisim against given .asm program, comparing your behaviours with MARS simulation.
+This helps to verify MIPS CPU circuits in Logisim against behaviours from MARS simulation of given .asm program.
 
 ### Usage
 
-Simply set up the output pins in your `main` circuit in order of PC (32-bit by default), GRF_WRITE_ENABLED (1-bit), GRF_WRITE_ADDRESS (5-bit), GRF_WRITE_DATA (32-bit), DM_WRITE_ENABLED (1-bit), DM_WRITE_ADDRESS (5-bit by default), DM_WRITE_DATA (32-bit), and halt (1-bit). The output pin labelled "halt" should be pulled to 1 when the entire program has finished, and all the other pins are not required to be labelled. Dumped instructions for verification will be loaded into the ROM component automatically.
+#### Setting up
+
+Set the output pins in your `main` circuit in order of PC (32-bit by default), GRF_WRITE_ENABLED (1-bit), GRF_WRITE_ADDRESS (5-bit), GRF_WRITE_DATA (32-bit), DM_WRITE_ENABLED (1-bit), DM_WRITE_ADDRESS (5-bit by default), DM_WRITE_DATA (32-bit), and halt (1-bit). The output pin labelled "halt" should be pulled to 1 when the entire program has finished, and all the other pins are not required to be labelled. Dumped instructions for verification will be loaded into the ROM component automatically.
 
 GRF_WRITE_ADDRESS and GRF_WRITE_DATA are ignored when judging if GRF_WRITE_ENABLED or GRF_WRITE_ADDRESS outputs 0, and the same goes for DM.
 
 #### CLI
 
 ```shell
-$ python judge.py p3.circ test.asm somewhere/logisim.jar kits/Mars_Changed.jar
+$ python judge.py p3.circ test.asm somewhere/logisim.jar
 ```
+
+This loads dumpped instructions into the Logisim project, runs MARS and Logisim simulations and checks the output. Both output files can be found in folder `./tmp`.
 
 A modified version of MARS which outputs all writing accesses is required, and also provided in this repo.
 
 ```
 $ python judge.py --help
 ```
-This shows extra options from CLI.
+This shows extra options of the CLI tool.
 
 #### Python APIs
 
-- `class Judge(logisim_path, mars_path='kits/Mars_Changed.jar', java_path='java', pc_width=32, dma_width=5, pc_by_word=False, dma_by_word=False)`
+- `class Judge(logisim_path, mars_path='kits/Mars_Changed.jar', java_path='java', pc_width=32, pc_by_word=False, pc_start=0x0000, dma_width=5, dma_by_word=False)`
 
-- `Judge.__call__(circ_path, asm_path, ifu_circ_name=None)`
+- `Judge.__call__(circ_path, asm_path, ifu_circ_name=None, logisim_timeout=1, mars_timeout=1)`
 
 ##### Example
 
@@ -35,6 +39,9 @@ import sys
 from judge import Judge
 
 judge = Judge('../logisim-generic-2.7.1.jar')
-if judge('p3.circ', 'mips1.asm'):
-    print('failed qwq', file=sys.stderr)
+try:
+    judge('p3.circ', 'mips1.asm')
+except Judge.VerificationFailed as e:
+    print('failed qwq:', e, file=sys.stderr)
+    sys.exit(1)
 ```
--- judge.py
+++ judge.py
@@ -1,9 +1,19 @@
 import os, os.path, sys, subprocess, string, argparse
 import xml.etree.ElementTree as ET
 from hashlib import md5
+from ast import literal_eval
 
 tmp_pre = 'tmp'
 diff_fn = 'fc' if os.name == 'nt' else 'diff'
+
+pc_width_default = 32
+pc_by_word_default = False
+pc_start_default = 0
+dma_width_default = 5
+dma_by_word_default = False
+logisim_timeout_default = 1
+mars_timeout_default = 1
+mars_path_default = os.path.join(os.path.dirname(__file__), 'kits', 'Mars_Changed.jar')
 
 hex_chars = set(filter(lambda c: not c.isupper(), string.hexdigits))
 def to_instr(line):
@@ -28,22 +38,27 @@ class LogLine:
         self.s = ''.join(line.split())
         for ch in self.s:
             if ch not in ('0', '1'):
-                raise RuntimeError('Bad output: ' + line)
+                raise ValueError('Bad output: ' + line)
         self.p = 0
 
-    def take(self, n):
+    def take(self, n, by_word=False):
         self.p += n
-        return int(self.s[self.p - n : self.p], 2)
+        r = int(self.s[self.p - n : self.p], 2)
+        if by_word:
+            r *= 4
+        return r
 
-    def take_hex(self, n):
-        return to_hex(self.take(n))
+    def take_hex(self, n, by_word=False):
+        return to_hex(self.take(n, by_word))
 
-    def parse(self, pc_width=32, dma_width=5, pc_by_word=False, dma_by_word=False):
-        pc = self.take(pc_width)
-        if pc_by_word:
-            pc = 0x3000 + pc * 4
-        pc = to_hex(pc)
-
+    def parse(self,
+        pc_width=pc_width_default,
+        pc_by_word=pc_by_word_default,
+        pc_start=pc_start_default,
+        dma_width=dma_width_default,
+        dma_by_word=dma_by_word_default
+    ):
+        pc = to_hex(0x3000 - pc_start + self.take(pc_width, pc_by_word))
         gw = self.take(1)
         ga_int = self.take(5)
         ga = to_dec(ga_int)
@@ -51,16 +66,10 @@ class LogLine:
         if gw and ga_int:
             return '@{}: ${} <= {}'.format(pc, ga, gd)
 
-        dw = self.take(1)
-        if dw:
-            da = self.take(dma_width)
-            if dma_by_word:
-                da *= 4
-            da = to_hex(da)
-            dd = self.take_hex(32)
-            return '@{}: *{} <= {}'.format(pc, da, dd)
+        if self.take(1):
+            da = self.take_hex(dma_width, dma_by_word)
+            return '@{}: *{} <= {}'.format(pc, da, self.take_hex(32))
         return None
-
 
 def gen_cpu(circ_path, prog_hex_fn, ifu_circ_name):
     tree = ET.parse(circ_path)
@@ -111,15 +120,18 @@ def gen_cpu(circ_path, prog_hex_fn, ifu_circ_name):
     tree.write(new_circ_path)
     return new_circ_path
 
-
 class Judge:
 
-    def __init__(self, logisim_path, mars_path='kits/Mars_Changed.jar',
+    class VerificationFailed(Exception):
+        pass
+
+    def __init__(self, logisim_path, mars_path=mars_path_default,
         java_path='java',
-        pc_width=32,
-        dma_width=5,
-        pc_by_word=False,
-        dma_by_word=False
+        pc_width=pc_width_default,
+        pc_by_word=pc_by_word_default,
+        pc_start=pc_start_default,
+        dma_width=dma_width_default,
+        dma_by_word=dma_by_word_default,
         ):
         if not os.path.isdir(tmp_pre):
             os.mkdir(tmp_pre)
@@ -129,39 +141,66 @@ class Judge:
         self.java_path = java_path
 
         self.pc_width = pc_width
-        self.dma_width = dma_width
         self.pc_by_word = pc_by_word
+        self.pc_start = pc_start
+        self.dma_width = dma_width
         self.dma_by_word = dma_by_word
 
-    def __call__(self, circ_path, asm_path, ifu_circ_name=None):
+    @staticmethod
+    def _mars_communicate(proc, fp, timeout=None):
+        for line in proc.communicate(timeout=timeout)[0].splitlines():
+            s = line.decode().strip()
+            if s.startswith('@'):
+                fp.write(s + '\n')
+
+    def _logisim_communicate(self, proc, fp, timeout=None):
+        for line in proc.communicate(timeout=timeout)[0].splitlines():
+            s = line.decode().strip()
+            try:
+                r = LogLine(s).parse(
+                    self.pc_width, self.pc_by_word, self.pc_start,
+                    self.dma_width, self.dma_by_word
+                )
+            except ValueError:
+                raise Judge.VerificationFailed('invalid circuit output: ' + s)
+            if r:
+                fp.write(r + '\n')
+
+    @staticmethod
+    def _call(cmd, out_fn, handler, timeout, timeout_msg, error_meta):
+        with open(out_fn, 'w', encoding='utf-8') as fp:
+            with subprocess.Popen(cmd, stdout=subprocess.PIPE) as proc:
+                try:
+                    handler(proc, fp, timeout)
+                except subprocess.TimeoutExpired as e:
+                    proc.kill()
+                    handler(proc, fp)
+                    raise Judge.VerificationFailed(timeout_msg.format(timeout)) from e
+                if proc.returncode:
+                    raise Judge.VerificationFailed(error_meta + ' subprocess returned ' + str(proc.returncode))
+
+    def __call__(self, circ_path, asm_path,
+        ifu_circ_name=None,
+        logisim_timeout=logisim_timeout_default,
+        mars_timeout=mars_timeout_default
+        ):
         hex_fn = os.path.join(tmp_pre, os.path.basename(asm_path) + '.hex')
         ans_fn = os.path.join(tmp_pre, os.path.basename(asm_path) + '.ans')
-
-        with open(ans_fn, 'w', encoding='utf-8') as fp:
-            with subprocess.Popen(
-                [self.java_path, '-jar', self.mars_path, asm_path,
-                    'mc', 'CompactDataAtZero', 'dump', '.text', 'HexText', hex_fn],
-                stdout=subprocess.PIPE) as proc:
-                for line in proc.stdout:
-                    s = line.decode()
-                    if s.startswith('@'):
-                        fp.write(s)
-            if proc.returncode:
-                raise RuntimeError('MARS process returned ' + str(proc.returncode))
+        self._call([self.java_path, '-jar', self.mars_path, asm_path,
+                'nc', 'mc', 'CompactDataAtZero', 'dump', '.text', 'HexText', hex_fn],
+            ans_fn, self._mars_communicate, mars_timeout,
+            'MARS simulation timed out (> {} secs), maybe a infinite loop, see ' + ans_fn, 'MARS'
+        )
 
         circ_path = gen_cpu(circ_path, hex_fn, ifu_circ_name)
         out_fn = os.path.join(tmp_pre, os.path.basename(asm_path) + '.out')
-        with open(out_fn, 'w', encoding='utf-8') as fp:
-            with subprocess.Popen([self.java_path, '-jar', self.logisim_path, circ_path, '-tty', 'table'], stdout=subprocess.PIPE) as proc:
-                for line in proc.stdout:
-                    r = LogLine(line.decode()).parse(self.pc_width, self.dma_width, self.pc_by_word, self.dma_by_word)
-                    if r:
-                        fp.write(r + '\n')
-            if proc.returncode:
-                raise RuntimeError('Logisim process returned ' + str(proc.returncode))
+        self._call([self.java_path, '-jar', self.logisim_path, circ_path, '-tty', 'table'],
+            out_fn, self._logisim_communicate, logisim_timeout,
+            'Logisim circuit timed out (> {} secs) before halting, see ' + out_fn, 'Logisim'
+        )
 
-        return subprocess.run([diff_fn, out_fn, ans_fn]).returncode
-
+        if subprocess.run([diff_fn, out_fn, ans_fn]).returncode:
+            raise Judge.VerificationFailed('output differs, see {} and {}'.format(out_fn, ans_fn))
 
 def dump_self():
     with open(__file__, 'rb') as inf, \
@@ -170,27 +209,38 @@ def dump_self():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run your CPU in Logisim and compare with results from MARS.')
+    parser = argparse.ArgumentParser(description='verify MIPS CPU circuits in Logisim against MARS simulation of given .asm program.')
     parser.add_argument('circ_path', help='path to your Logisim project file')
     parser.add_argument('asm_path', help='path to the .asm program to load into circuit and run in MARS')
     parser.add_argument('logisim_path', help='path to local Logisim .jar file')
     parser.add_argument('mars_path', nargs='?', help='path to local MARS .jar file, "kits/Mars_Changed.jar" by default',
-                        default='kits/Mars_Changed.jar')
+                        default=mars_path_default)
     parser.add_argument('--java_path', metavar='path',
                         default='java', help='path to your jre binary, omit this if java is in your path environment')
     parser.add_argument('--ifu_circ_name', metavar='ifu',
-                        default=None, help='name of the circuit containing the ROM to load dumped instructions, omit to search in all circuits')
+                        default=None, help='name of the circuit containing the ROM to load dumped instructions, omit to find in the whole project')
     parser.add_argument('--pc_width', metavar='width',
-                        default=32, help='width of PC in CPU output, 32 by default')
-    parser.add_argument('--dm_addr_width', metavar='width',
-                        default=5, help='width of written DM address in output, 5 by default')
+                        default=pc_width_default, help='width of output PC, {} by default'.format(pc_by_word_default))
+    parser.add_argument('--pc_start', metavar='addr',
+                        default=str(pc_start_default), help='starting address of output PC, {} by default'.format(hex(pc_start_default)))
     parser.add_argument('--pc_by_word', action='store_true',
-                        help='specify this if output PC is word addressing (0, 1, 2, ..), otherwise it should be compatible with MARS (0x3000, 0x3004, ...)')
+                        help='specify this if output PC is word addressing')
+    parser.add_argument('--dm_addr_width', metavar='width',
+                        default=dma_width_default, help='width of DM_WRITE_ADDRESS in output, {} by default'.format(dma_width_default))
     parser.add_argument('--dm_addr_by_word', action='store_true',
                         help='specify this if output DM address is word addressing')
+    parser.add_argument('--logisim_timeout', metavar='secs',
+                        default=logisim_timeout_default, help='timeout for Logisim simulation, {} by default'.format(logisim_timeout_default))
+    parser.add_argument('--mars_timeout', metavar='secs',
+                        default=mars_timeout_default, help='timeout for MARS simulation, {} by default'.format(mars_timeout_default))
 
     args = parser.parse_args()
-    judge = Judge(args.logisim_path, args.mars_path, args.java_path, args.pc_width, args.dm_addr_width, args.pc_by_word, args.dm_addr_by_word)
-    if judge(args.circ_path, args.asm_path, args.ifu_circ_name):
-        print('Differs!', file=sys.stderr)
+    judge = Judge(args.logisim_path, args.mars_path, args.java_path,
+        args.pc_width, args.pc_by_word, int(literal_eval(args.pc_start)),
+        args.dm_addr_width, args.dm_addr_by_word)
+    try:
+        judge(args.circ_path, args.asm_path, args.ifu_circ_name, args.logisim_timeout, args.mars_timeout)
+    except Judge.VerificationFailed as e:
+        print(e, file=sys.stderr)
         sys.exit(1)
+    print('ok')
