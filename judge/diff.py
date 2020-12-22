68a8422a6142f55e51a1bf7f9c3f9d02eb17a7a6
karin0 <karin0@gmx.com>
2020-12-23 01:13:46 +0800

    feat(diff): add a switch to compare as a prefix

--- judge/diff.py
+++ judge/diff.py
@@ -10,23 +10,37 @@ class InconsistentResults(VerificationFailed):
 
 class Diff:
 
-    def __init__(self, diff_path=None, keep_output_files=False):
+    def __init__(self, diff_path=None, keep_output_files=False, allow_prefix=False):
         self.diff_path = diff_path_default if diff_path is None else diff_path
         self.keep_output_files = keep_output_files
+        self.allow_prefix = allow_prefix
 
     def __call__(self, out_path, ans_path, log_path=None):
+        def complain():
+            nonlocal log_path
+            if log_path is None:
+                log_path = out_path + '.diff'
+            with open(log_path, 'wb') as fp:
+                fp.write(res[0] + b'\n' + res[1])
+            raise InconsistentResults('output differs, see {}, {}, and {} for diff logs'
+                                      .format(out_path, ans_path, log_path))
+
         with subprocess.Popen([self.diff_path, out_path, ans_path],
                               stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
             res = proc.communicate()
             if proc.returncode:
-                if log_path is None:
-                    log_path = out_path + '.diff'
-                with open(log_path, 'wb') as fp:
-                    fp.write(res[0] + b'\n' + res[1])
                 # res = res[0].decode(errors='ignore') + res[1].decode(errors='ignore')
                 # print(res, file=sys.stderr)
-                raise InconsistentResults('output differs, see {}, {}, and {} for diff logs'
-                                          .format(out_path, ans_path, log_path))
-            elif not self.keep_output_files:
-                os.remove(out_path)
-                os.remove(ans_path)
+                if self.allow_prefix:
+                    with open(out_path, 'rb') as fp1, open(ans_path, 'rb') as fp2:
+                        s1 = fp1.read().strip()
+                        s2 = fp2.read().strip()
+                    l = min(len(s1), len(s2))
+                    if s1[:l] != s2[:l]:
+                        return complain()
+                else:
+                    return complain()
+
+        if not self.keep_output_files:
+            os.remove(out_path)
+            os.remove(ans_path)
